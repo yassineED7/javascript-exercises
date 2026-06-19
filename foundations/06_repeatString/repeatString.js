817c41686a2525aa9555f9b1ffeb9c835eef40ee
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-19 16:14:48 +0100

    Solves repeatString exercise and passes all tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    if (num < 0){return 'ERROR'}
+    result = '';
+    for (let i = 0; i < num; i++){
+        result += str;
+    }
+    return result;
 };
+
+repeatString('hey', 3)
+// console.log(repeatString('hey', 3))
+
 
 // Do not edit below this line
 module.exports = repeatString;
