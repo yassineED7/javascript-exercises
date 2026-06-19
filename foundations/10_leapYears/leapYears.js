0c17d33ac559cdb043ea04baac457b46cc88f942
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-19 19:26:35 +0100

    Solves tempConversion exercise with precise floating-point rounding

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if ((years%4 === 0 && years%100 != 0 || years%400 === 0)){
+        return true;
+    }else {return false;}
 };
+
+leapYears(2000); // is a leap year: returns true
+leapYears(1985); // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatures) {
+  let celsius =  ((temperatures - 32)*(5/9));
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatures) {
+  let fahrenheit = ((temperatures *(9/5))+32);
+  return Math.round(fahrenheit * 10) / 10;
+
 };
+
+convertToCelsius(32) // fahrenheit to celsius, should return 0
+convertToFahrenheit(0) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
