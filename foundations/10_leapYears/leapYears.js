abc1a5c5a886ef4c6232b0721c0fa5d62722599a
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-19 19:28:17 +0100

    Solves leapYears exercise using logical operators

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -2,6 +2,7 @@ const leapYears = function(years) {
     if ((years%4 === 0 && years%100 != 0 || years%400 === 0)){
         return true;
     }else {return false;}
+    
 };
 
 leapYears(2000); // is a leap year: returns true
