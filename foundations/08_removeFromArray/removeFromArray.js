2f885eaa0ed834ece7d97f5235fb6b3bfee63d1f
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-19 16:54:10 +0100

    Solves removeFromArray exercise using rest parameters and filter

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    return arr.filter(item => !arg.includes(item));
+
 };
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
