625d077422ad6de1dfc2bf986284d265ce7cdb34
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-19 16:29:39 +0100

    Solves reverseString exercise and passes all tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    return word
+            .split('')
+            .reverse()
+            .join('')
 };
+
+reverseString('hello there') // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
