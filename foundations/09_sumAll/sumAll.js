c6483979541b1ed00d74f7f19b8b78ce6e1715c5
yassine-ed <yassine.eddaadoua37@gmail.com>
2026-06-19 18:01:19 +0100

    Solves sumAll exercise with strict input validation and variable swapping

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    let sum = 0;
+    let cup
+    if ((min >= 0 && max >= 0)
+        && (Number.isInteger(min) && Number.isInteger(max))) {
+        if (min > max) { cup = min; min = max; max = cup; }
 
+        for (let i = min; i <= max; i++) {
+            sum = i + sum;
+        }
+        return sum;
+    } else { return 'ERROR'; }
 };
+
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 
 // Do not edit below this line
 module.exports = sumAll;
